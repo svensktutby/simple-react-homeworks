a310912ffc8df7fbacc4e8fbb8b9db666ef7b606
svensktutby <svensk.tut.by@gmail.com>
2020-11-26 22:53:31 +0300

    feat: resolve hw3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,38 +1,42 @@
-import React, {useState} from "react";
-import Greeting from "./Greeting";
+import React, { ChangeEvent, FC, useState } from 'react';
+import Greeting from './Greeting';
+import { UserType } from './HW3';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
-}
+  users: Array<UserType>; // need to fix any
+  addUserCallback: (name: string) => void; // need to fix any
+};
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>(""); // need to fix any
-    const [error, setError] = useState<any>(""); // need to fix any
+const GreetingContainer: FC<GreetingContainerPropsType> = ({ users, addUserCallback }) => {
+  // деструктуризация пропсов
+  const [name, setName] = useState<string>(''); // need to fix any
+  const [error, setError] = useState<boolean>(false); // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName(""); // need to fix
-    };
-    const addUser = () => {
-        alert(`Hello  !`); // need to fix
-    };
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+    const userName = e.currentTarget.value.trim();
+    setName(userName); // need to fix
+  };
+  const addUser = () => {
+    if (name) {
+      addUserCallback(name);
+      alert(`Hello ${name}!`); // need to fix
+      setError(false);
+      setName('');
+    } else {
+      setError(true);
+    }
+  };
 
-    const totalUsers = 0; // need to fix
+  const totalUsers = users.length; // need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    );
-}
+  return (
+    <Greeting name={name} setNameCallback={setNameCallback} addUser={addUser} error={error} totalUsers={totalUsers} />
+  );
+};
 
 export default GreetingContainer;
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,34 +1,37 @@
-import React, {useState} from "react";
-import GreetingContainer from "./GreetingContainer";
+import React, { useState } from 'react';
+import {v1} from 'uuid';
+import GreetingContainer from './GreetingContainer';
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
-}
+  _id: string; // need to fix any
+  name: string; // need to fix any
+};
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]); // need to fix any
+  const [users, setUsers] = useState<Array<UserType>>([]); // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]); // need to fix
-    }
+  const addUserCallback = (name: string) => {
+    // need to fix any
+    const user = {
+      _id: v1(),
+      name,
+    };
+    setUsers([...users, user]); // need to fix
+  };
 
-    return (
-        <div>
-            <hr/>
-            homeworks 3
-
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeGreeting/>*/}
-            <hr/>
-        </div>
-    );
+  return (
+    <div>
+      <hr />
+      homeworks 3{/*should work (должно работать)*/}
+      <GreetingContainer users={users} addUserCallback={addUserCallback} />
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeGreeting/>*/}
+      <hr />
+    </div>
+  );
 }
 
 export default HW3;
