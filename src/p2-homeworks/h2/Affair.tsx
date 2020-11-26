c3f1ea79aa421dab67184a9edc54e85a8b60302b
svensktutby <svensk.tut.by@gmail.com>
2020-11-26 19:07:17 +0300

    refactor: change styles hw2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -10,7 +10,7 @@ type AffairPropsType = {
 };
 
 function Affair(props: AffairPropsType) {
-  const { affair, btn, btnClose } = classes;
+  const { affair, btn, btnClose, icon } = classes;
 
   const deleteCallback = () => {
     props.deleteAffairCallback(props.affair._id);
@@ -19,7 +19,13 @@ function Affair(props: AffairPropsType) {
   return (
     <li className={affair}>
       <span>{props.affair.name}</span>
-      <button className={`${btn} ${btnClose}`} onClick={deleteCallback}>×</button>
+      {/*<button className={`${btn} ${btnClose}`} onClick={deleteCallback}>×</button>*/}
+      <button className={`${btn} ${btnClose}`} onClick={deleteCallback}>
+        <svg className={icon} version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32">
+          <path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path>
+          <path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path>
+        </svg>
+      </button>
     </li>
   );
 }
