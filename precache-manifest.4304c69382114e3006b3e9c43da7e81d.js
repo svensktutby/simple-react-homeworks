b626443c9edf1c690e1c60adbc2e2bb9b21fefe2
svensktutby <svensk.tut.by@gmail.com>
2020-12-07 12:28:10 +0300

    Updates

---
+++ precache-manifest.4304c69382114e3006b3e9c43da7e81d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f3a18713f601fff5cb0e382480f3b5",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "6cd5825da00e57cc22cc",
+    "url": "/simple-react-homeworks/static/css/main.8fc7fe9d.chunk.css"
+  },
+  {
+    "revision": "5f6ed6ec8fa686e10e34",
+    "url": "/simple-react-homeworks/static/js/2.6af045d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks/static/js/2.6af045d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd5825da00e57cc22cc",
+    "url": "/simple-react-homeworks/static/js/main.0f7f07c0.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  }
+]);
