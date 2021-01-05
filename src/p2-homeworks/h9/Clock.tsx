bd3082fa9340645b890046b1c9958cade63ba8af
svensktutby <svensk.tut.by@gmail.com>
2021-01-05 15:00:32 +0300

    feat: resolve hw9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,52 +1,59 @@
-import React, {useState} from "react";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import React, { useState } from 'react';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
+import s from './Clock.module.css';
+
+const padTime = (time: number): string => time.toString().padStart(2, '0');
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
-    const [show, setShow] = useState<boolean>(false);
+  const [timerId, setTimerId] = useState<number>(0);
+  const [date, setDate] = useState<Date>(new Date());
+  const [show, setShow] = useState<boolean>(false);
 
-    const stop = () => {
-        // stop
-    }
-    const start = () => {
-        stop();
-        const id: number = window.setInterval(() => {
-            // setDate
-        }, 1000);
-        setTimerId(id);
-    }
+  const stop = () => {
+    clearInterval(timerId);
+  };
+  const start = () => {
+    stop();
+    const id: number = window.setInterval(() => {
+      setDate(new Date())
+    }, 1000);
+    setTimerId(id);
+  };
 
-    const onMouseEnter = () => {
-        // show
-    };
-    const onMouseLeave = () => {
-        // close
-    };
+  const onMouseEnter = () => {
+    setShow(true);
+  };
+  const onMouseLeave = () => {
+    setShow(false);
+  };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+  const stringTime = `${padTime(
+    date.getHours())}:${padTime(
+    date.getMinutes())}:${padTime(
+    date.getSeconds())}`; // fix with date
+  const stringDate = `${padTime(
+    date.getDate())}-${padTime(
+    date.getMonth() + 1,
+  )}-${date.getFullYear()}`; // fix with date
 
-    return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+  return (
+    <div className={s.clock}>
+      <div
+        className={s.time}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {stringTime}
+      </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+      {show && <div className={s.date}>{stringDate}</div>}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
-        </div>
-    );
+      <div className={s.actions}>
+        <SuperButton onClick={start}>start</SuperButton>
+        <SuperButton onClick={stop}>stop</SuperButton>
+      </div>
+    </div>
+  );
 }
 
 export default Clock;
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Clock from "./Clock";
+import React from 'react';
+import Clock from './Clock';
+import s from './HW9.module.css';
 
 function HW9() {
-    return (
-        <div>
-            <hr/>
-            homeworks 9
-
-            {/*should work (должно работать)*/}
-            <Clock/>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
-            <hr/>
-        </div>
-    );
+  return (
+    <div className={s.taskWrapper}>
+      <hr />
+      homeworks 9{/*should work (должно работать)*/}
+      <div className={s.wrapper}>
+        <Clock />
+      </div>
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeClock/>*/}
+      <hr />
+    </div>
+  );
 }
 
 export default HW9;
